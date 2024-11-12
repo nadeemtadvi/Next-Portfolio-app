c19e63ec1072d2948d7d877f5535e42ab50bddaf
nadeemtadvi <nadeemtadvi1@gmail.com>
2024-11-13 01:23:27 +0530

    change icon footer

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -1,45 +1,33 @@
 import React from "react";
 import { Footer } from "../constant/constant";
-import insta from '../assets/Icons/social.png'
-import git from '../assets/Icons/github.png'
-import Linkedin from '../assets/Icons/linkedin.png'
-import facebook from '../assets/Icons/facebook (1).png'
-import Image from "next/image";
 import Link from "next/link";
-
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaSquareWhatsapp } from "react-icons/fa6";
+import { FaSquareGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 const FooterSection = () => {
   return (
     <div className="footer border-t border-gray-300">
       <div className="p-5 flex justify-between items-center">
-        <h4 className="text-[0.8rem] text-subtext dark:text-darksubtext">{Footer.copyright}</h4>
-        <h4 className="text-[0.8rem] text-subtext dark:text-darksubtext">{Footer.dev}</h4>
-        <div className="flex-wrap flex justify-between items-center gap-1 sm:gap-5">
-          <Image
-          src={insta}
-          alt='insta'
-          width={24}
-          height={24}
-          />
-          <Image
-          src={facebook}
-          alt='insta'
-          width={24}
-          height={24}
-          />
-         <Link href="https://www.linkedin.com/in/nadeem-tadvi-290339159" target="_blank"> <Image
-          src={Linkedin}
-          alt='insta'
-          width={24}
-          height={24}
-          /></Link>
-          <Link href="https://github.com/nadeemtadvi" target="_blank"><Image
-          src={git}
-          alt='insta'
-          width={24}
-          height={24}
-          />
-        </Link>
+        <h4 className="text-[0.8rem] text-subtext dark:text-darksubtext">
+          {Footer.copyright}
+        </h4>
+        <h4 className="text-[0.8rem] text-subtext dark:text-darksubtext">
+          {Footer.dev}
+        </h4>
+        <div className="flex-wrap flex justify-between items-center gap-1 sm:gap-5 text-mainColor dark:text-white text-[24px]">
+          <FaSquareInstagram />
+          <FaSquareWhatsapp />
+          <Link href="https://github.com/nadeemtadvi" target="_blank">
+            <FaSquareGithub />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/nadeem-tadvi-290339159"
+            target="_blank"
+          >
+            <FaLinkedin />
+          </Link>
         </div>
       </div>
     </div>
